d359ef4b199ced4dd3d24d92b3880f0fb1a6aab7
thequietprotocol <thequietprotocol@proton.me>
2026-03-04 00:01:53 +0100

    test case for each instr type

--- simulation/decoder_tb.sv
+++ simulation/decoder_tb.sv
@@ -26,10 +26,29 @@ module decoder_tb;
   );
 
   initial begin
-    // Test 1: ADDI R1, R2, 5
+    // Test 1: ADDI R1, R2, 5 - imm[11:0] | rs1 | f3  | rd | opcode
     instr = 32'h00510093; #10;
     $display("ADDI: opcode=%b rd=%0d rs1=%0d imm=%0d", opcode, rd, rs1, imm);
 
+    // Test 2: ADD R3, R1, R2 - funct7 | rs2 | rs1 | f3  | rd  | opcode
+    instr = 32'h002080B3; #10;
+    $display("ADD: opcode=%b rd=%0d rs1=%0d rs2=%0d", opcode, rd, rs1, rs2);
+
+    // Test 3: SW R2, 8(R1) - imm[11:5] | rs2 | rs1 | f3 | imm[4:0] | opcode
+    instr = 32'h0020A423; #10;
+    $display("ADD: opcode=%b imm=%0d rs1=%0d rs2=%0d", opcode, imm, rs1, rs2);
+
+    // Test 4: BEQ R1, R2, 16 - imm[12|10:5] | rs2 | rs1 | f3  | imm[4:1|11] | opcode
+    instr = 32'h00208863; #10;
+    $display("ADD: opcode=%b imm=%0d rs1=%0d rs2=%0d", opcode, imm, rs1, rs2);
+
+    // Test 5: LUI R7, 0xABCDE - imm[31:12] | rd | opcode
+    instr = 32'hABCDE3B7; #10;
+    $display("ADD: opcode=%b imm=%0d rd=%0d", opcode, imm, rd);
+
+    // Test 6: JAL R1, 100 - imm[20|10:1|11|19:12] | rd | opcode
+    instr = 32'h064000EF; #10;
+    $display("ADD: opcode=%b imm=%0d rd=%0d", opcode, imm, rd);
 
     $finish;
   end
